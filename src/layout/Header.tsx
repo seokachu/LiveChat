15a885c688e3d0c6bb17d5604ef7a7bd565036ad
박서영 <seokachuu@gmail.com>
2024-08-19 23:11:19 +0900

    [Style] header css 완성

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -9,6 +9,7 @@ import {
 import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "react-toastify";
+import S from "@/style/header.module.css";
 
 const Header = () => {
   const router = useRouter();
@@ -26,17 +27,19 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <h1>내 닉네임 : {nickname}</h1>
-      <h2>현재 접속 상태 : {isConnect ? "입장" : "미입장"}</h2>
-      <button
-        onClick={() => {
-          disConnectToChatServer();
-        }}
-      >
-        접속종료
-      </button>
-    </header>
+    <div className={S.headerWrapper}>
+      <header className={S.header}>
+        <h1>내 닉네임 : {nickname}</h1>
+        <h2>현재 접속 상태 : {isConnect ? "입장" : "미입장"}</h2>
+        <button
+          onClick={() => {
+            disConnectToChatServer();
+          }}
+        >
+          접속종료
+        </button>
+      </header>
+    </div>
   );
 };
 
